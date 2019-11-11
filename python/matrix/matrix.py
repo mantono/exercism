a3004183a1c420499bd1849d268eafff9f550511
Anton Österberg <anton.osterberg@gmail.com>
2019-11-11 17:54:09 +0100

    matrix: Use list comprehension in create_columns

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -17,12 +17,6 @@ class Matrix(object):
         return self.columns[index-1]
 
 
-# 9 8 7 | 0 1 2
-# 5 3 2 | 3 4 5
-# 6 6 7 | 6 7 8
-
-# 1 -> [9, 8, 7]
-# 2 -> [5, 3, 2]
 def create_rows(matrix_string: str) -> List[List[int]]:
     matrix: List[List[int]] = []
 
@@ -35,16 +29,7 @@ def create_rows(matrix_string: str) -> List[List[int]]:
     return matrix
 
 
-# 9 8 7 | 9 5 6
-# 5 3 2 | 8 3 6
-# 6 6 7 | 7 2 7
-
-# 1 -> 9 5 6
-# 3 -> 7 2 7
 def create_columns(matrix_string: str) -> List[List[int]]:
     rows: List[List[int]] = create_rows(matrix_string)
-    matrix: List[List[int]] = []
-    for column in zip(*rows):
-        matrix.append(list(column))
-
+    matrix: List[List[int]] = [list(column) for column in zip(*rows)]
     return matrix
