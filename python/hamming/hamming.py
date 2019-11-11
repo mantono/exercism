41a12486e8ffad9ee6395565308474e5394d3566
Anton Österberg <anton.osterberg@gmail.com>
2019-11-11 16:49:32 +0100

    Add python/hamming (work in progress)

---
+++ python/hamming/hamming.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def distance(strand_a: List[object], strand_b: List[object]) -> int:
+    if len(strand_a) != len(strand_b):
+        raise Exception("Inputs have different length")
+    length: int = len(strand_a)
+    matches: int = 0
+    for s0, s1 in zip(strand_a, strand_b):
+        if s0 == s1:
+            matches += 1
+
+    return length - matches
