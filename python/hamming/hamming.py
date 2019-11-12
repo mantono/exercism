c0a92f08b4b09762e89e295746ea35b5039a6c67
Anton Österberg <anton.osterberg@gmail.com>
2019-11-12 12:26:43 +0100

    hamming: Raise the right sort of error/exception

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -3,7 +3,7 @@ from typing import List
 
 def distance(strand_a: List[object], strand_b: List[object]) -> int:
     if len(strand_a) != len(strand_b):
-        raise Exception("Inputs have different length")
+        raise ValueError("Inputs have different length")
     length: int = len(strand_a)
     matches: int = 0
     for s0, s1 in zip(strand_a, strand_b):
