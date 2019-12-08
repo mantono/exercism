517f2f17979f7d2364c2376eb3e9db82862ca960
Anton Österberg <anton.osterberg@gmail.com>
2019-12-08 23:05:57 +0100

    Primes: Adapt a slightly more functional approach

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -25,24 +25,18 @@ fn is_prime(known_primes: &Vec<u32>, n: u32) -> bool {
         return false
     }
 
-    let until: u32 = (n as f64).sqrt() as u32;
+    let limit: u32 = (n as f64).sqrt() as u32;
+    let certain_composite: bool = known_primes.iter()
+        .take_while(|prime| prime <= &&limit)
+        .any(|&prime| n % prime == 0);
 
-    for prime in known_primes.iter() {
-        if n % prime == 0 {
-            return false;
-        }
-        if prime >= &until {
-            break;
-        }
+    if certain_composite {
+        return false
     }
 
-    let mut num: u32 = if until % 2 == 0 {
-        until + 1
-    } else {
-        until
-    };
+    let mut num: u32 = coerce_to_odd(limit);
 
-    while num <= until {
+    while num <= limit {
         if n % num == 0 {
             return false
         }
@@ -50,4 +44,12 @@ fn is_prime(known_primes: &Vec<u32>, n: u32) -> bool {
     }
 
     return true
+}
+
+fn coerce_to_odd(n: u32) -> u32 {
+    if n % 2 == 0 {
+        n + 1
+    } else {
+        n
+    }
 }
